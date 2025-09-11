1c3089c730b3789d02b97a934af3099dcb965d1f
Khushi Yadav <kky957242@gmail.com>
2025-09-11 21:15:35 +0530

    Create Introduction.py

---
+++ Introduction.py
@@ -0,0 +1,7 @@
+Matplotlib- it is the data visualization library in python that 
+allow you to create a wide variety of plots such as - line plot,bar charts,scatter plot, pie chart etc.
+
+                                                               
+How to import
+import matplotlib.pyplot as plt
+                                                                                                                               
