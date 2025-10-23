360e6c2707bc9d3f8d642acbceceeb2e93ca42c2
Khushi Yadav <kky957242@gmail.com>
2025-10-23 18:33:13 +0530

    Add files via upload

---
+++ newplot.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+x = [1, 2, 3, 4, 5]
+y1 = [2, 4, 6, 8, 10]
+y2 = [1, 4, 9, 16, 25]
+
+plt.subplot(1, 2, 1)   
+plt.plot(x, y1, "r-")
+plt.title("Linear")
+
+plt.subplot(1, 2, 2)  
+plt.plot(x, y2, "b--")
+plt.title("Square")
+
+plt.show()
